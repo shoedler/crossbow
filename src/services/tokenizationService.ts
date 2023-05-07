91730b2294e9c3088d8ba47ca685d486e7f426ff
Simon Schödler <simon.schoedler@hotmail.ch>
2023-05-07 21:47:16 +0200

    Refactors tokenization, adds tokenizer tests

--- src/services/tokenizationService.ts
+++ src/services/tokenizationService.ts
@@ -11,56 +11,81 @@
 // GNU General Public License for more details.
 
 import { Editor, EditorPosition } from 'obsidian';
-import { stripMarkdown } from 'src/util';
 
 export type WordLookup = { [key: string]: EditorPosition[] };
+
+const OBSIDIAN_METADATA_REGEX =          /^\n*?---[\s\S]+?---/g;
+const OBSIDIAN_TAG_REGEX =               /#+([a-zA-Z0-9_/]+)/g;
+const OBSIDIAN_LINKS_REGEX =             /\[([^\]]+)\]+/g;
+
+const HTML_COMMENT_REGEX =               /<!--[\s\S]+?-->/g;
+const HTML_TAG_REGEX =                   /<\/?[\w\s="/.':;#-\/\?]+>/gm;
+
+const MARKDOWN_LATEX_BLOCK_REGEX =       /\$\$([^$]+)\$\$/g;
+const MARKDOWN_LATEX_INLINE_REGEX =      /\$([^$]+)\$/g;
+const MARKDOWN_LINKS_AND_IMAGES_REGEX =  /!?\[([^\]]+)\]\((?:<.*>)?\s*([^\s)]+)\s*\)/gm;
+const MARKDOWN_CODE_BLOCK_REGEX =        /```[\s\S]+?```/g;
+// const MARKDOWN_ASTERISK_EMPHASIS_REGEX = /([\*]+)(\S)(.*?\S)??\1/g;
+// const MARKDOWN_LODASH_EMPHASIS_REGEX =   /(^|\W)([_]+)(\S)(.*?\S)??\2($|\W)/g;
+// const MARKDOWN_CODE_INLINE_REGEX =       /`(.+?)`/g;
+// const MARKDOWN_STRIKETROUGH_REGEX =      /~(.*?)~/g;
 
 export class CrossbowTokenizationService {
   public constructor() {}
 
+  private readonly SKIP_REGEX = /\s/
+
   public getWordLookupFromEditor(targetEditor: Editor): WordLookup {
     if (!targetEditor) return {};
+    
+    const wordLookup: WordLookup = {};
 
-    const plainText = targetEditor.getValue();
-    const words: { word: string; pos: EditorPosition }[] = [];
-
+    const rawText = targetEditor.getValue();
+    const plainText = CrossbowTokenizationService.redactText(rawText);
+    
     for (let i = 0; i < plainText.length; i++) {
-      if (plainText[i].match(/\s/)) continue;
+      if (plainText[i].match(this.SKIP_REGEX)) 
+        continue;
       else {
         let word = '';
         let pos = targetEditor.offsetToPos(i);
 
-        while (plainText[i] && !plainText[i].match(/\s/)) word += plainText[i++];
+        while (plainText[i] && !plainText[i].match(this.SKIP_REGEX)) word += plainText[i++];
 
-        words.push({ word, pos });
+        word = CrossbowTokenizationService.cleanWord(word);
+
+        if (word.length  <= 0)
+          continue;
+
+        if (word in wordLookup) 
+          wordLookup[word].push(pos);
+        else 
+          wordLookup[word] = [pos];
       }
-    }
-
-    // Remove Headings and obsidian links
-    const filteredWords = words.filter((w) => {
-      if (w.word.length <= 0) return false;
-      if (w.word.startsWith('[[') && w.word.endsWith(']]')) return false;
-      if (w.word.startsWith('#')) return false;
-
-      return true;
-    });
-
-    // Create a lookup table for the words, where the key is the word and the value is an array of positions (occurrences of the word)
-    const wordLookup: { [key: string]: EditorPosition[] } = {};
-    for (let i = 0; i < filteredWords.length; i++) {
-      const word = stripMarkdown(filteredWords[i].word);
-
-      // Remove obsidian links which are longer than one word
-      if (filteredWords[i].word.startsWith('[[')) {
-        while (!filteredWords[++i].word.endsWith(']]') || i >= filteredWords.length) {
-          /* nothing */
-        }
-      }
-
-      if (word in wordLookup) wordLookup[word].push(filteredWords[i].pos);
-      else wordLookup[word] = [filteredWords[i].pos];
     }
 
     return wordLookup;
+  }
+
+  public static redactText(text: string): string {
+    const muteString = (str: string): string => str.replace(/[^\r\n]+/g, m => ' '.repeat(m.length));
+
+    // Order matters here
+    return text
+    .replace(MARKDOWN_CODE_BLOCK_REGEX, m => muteString(m))
+    .replace(MARKDOWN_LATEX_BLOCK_REGEX, m => muteString(m))
+    .replace(MARKDOWN_LATEX_INLINE_REGEX, m => muteString(m))
+    .replace(MARKDOWN_LINKS_AND_IMAGES_REGEX, m => muteString(m))
+    .replace(OBSIDIAN_METADATA_REGEX, m => muteString(m))
+    .replace(OBSIDIAN_TAG_REGEX, m => muteString(m))
+    .replace(OBSIDIAN_LINKS_REGEX, m => muteString(m))
+    .replace(HTML_COMMENT_REGEX, m => muteString(m))
+    .replace(HTML_TAG_REGEX, m => muteString(m))
+  }
+
+ public static cleanWord(word: string): string {
+  return word
+    .replace(/[^a-z0-9äöü'-]/gi, '') // Remove all non-alphanumeric characters except hyphens and apostrophes
+    .replace(/^[’'-]+|[’'-]+$/gi, ''); // Remove leading and trailing hyphens and apostrophes
   }
 }
